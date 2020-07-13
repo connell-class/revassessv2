332b9f23a4d5a592063a6a8900ed57eff6a143f1
connellrobert <robert.lel.connell@gmail.com>
2020-07-13 12:13:25 -0400

    fix issue 46 in original repo

--- Revassess/src/test/java/com/revature/tier5/answers/Answer1Tests.java
+++ Revassess/src/test/java/com/revature/tier5/answers/Answer1Tests.java
@@ -2,6 +2,7 @@ package com.revature.tier5.answers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,22 +26,22 @@ import static com.revature.tier5.answers.PointsTests.addPoints;
  */
 public class Answer1Tests {
 
-    String[] sources = new String[5];
+    String sources;
     @Before
     public void setup(){
-            sources[0]="https://code.jquery.com/jquery-3.4.1.slim.min.js";
-            sources[1]="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js";
-            sources[2]="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js";
-            sources[3]="js/index.js";
-            sources[4]="../js/index.js";
+            sources="https://code.jquery.com/jquery-[0-9].[0-9].[0-9].slim.min.js|"+
+                "https://cdn.jsdelivr.net/npm/popper.js@[0-9].[0-9]+.[0-9]/dist/umd/popper.min.js|"+
+                "https://stackpath.bootstrapcdn.com/bootstrap/[0-9].[0-9].[0-9]/js/bootstrap.min.js|"+
+                "js/index.js|"+
+                "../js/index.js";
     }
     @Test
     public void testScriptTags() {
         try {
             Document html = Jsoup.parse(new File("src/main/webapp/html/index.html"), "UTF-8");
             Elements scripts = html.getElementsByTag("script");
-            assertEquals(4, scripts.size());
-            scripts.forEach(e->assertNotEquals(-1,Arrays.asList(sources).indexOf(e.attr("src"))));
+            assertEquals(3, scripts.size());
+            scripts.forEach(e->assertTrue(e.attr("src").matches(sources)));
         } catch (IOException e) {
             e.printStackTrace();
         }
